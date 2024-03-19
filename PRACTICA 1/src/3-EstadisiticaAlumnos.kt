76b1131a5459949955bd4b9cc8df53c388e0765b
Carlos Mijail Mamani Anccasi <cmamania@ulasalle.edu.pe>
2024-03-19 09:35:20 -0500

    Ejercicio 3 mejorado!, Se uso temporales en lugar de Arrays, ademas para almacenar las edades se uso CADENAS.

--- PRACTICA 1/src/3-EstadisiticaAlumnos.kt
+++ PRACTICA 1/src/3-EstadisiticaAlumnos.kt
@@ -1,16 +1,16 @@
 /**
- * @problemDescription Se solicita saber cunatos alumnos hay en un salon, preguntar la eda de cada uno de estos,
- * saber el maximo y mino de edad y mostrar la edad de todos.
+ * @problemDescription Se solicita saber cuantos alumnos hay en un salon, preguntar la eda de cada uno de estos,
+ * saber el maximo y mino de edad y mostrar la edad de todos. Implementar sin usar LISTAS.
  * @author Carlos Mijail Mamani Anccasi
  * @creationDate 16/03/24
- * @lastModification ?
+ * @lastModification 19/03/24
  */
 
 fun ingreseAlumnos(cantidad: Int) {
     println("Ingrese la EDAD de cada alumno")
     val edadesAlumnos = IntArray(cantidad)
     for (i in 0 until cantidad) {
-        print("Alumno: ${i+1} ")
+        print("Alumno ${i+1}: ")
         edadesAlumnos[i] = readln().toInt()
     }
     println("\nEJEMPLO: $cantidad Alumnos")
@@ -36,14 +36,48 @@ fun calcularEdadMaximaMinima(alumnos: IntArray) {
 
 fun imprimirTodasLasEdades(alumnos: IntArray) {
     print("Todos = ")
-    for(edadActual in alumnos) {
-        print("$edadActual,")
+    for(edadActual in alumnos.dropLast(1)) {
+        print("$edadActual, ")
     }
+    println(alumnos.last())
+
 }
 
+/*------SIN USAR ARRAYS(LISTAS)------*/
+
+fun realizarEstadisticaSinListas(cantidad: Int) {
+    var edadActual: Int
+    var edadMaxima = 0
+    var edadMinima = Int.MAX_VALUE
+    var todasLasEdades = ""
+
+    println("Ingrese la EDAD de cada alumno")
+    for (i in 1..cantidad) {
+        print("Alumno $i: ")
+        edadActual = readln().toInt()
+        todasLasEdades += "$edadActual, "
+
+        if (edadActual > edadMaxima) {
+            edadMaxima = edadActual
+        }
+        if (edadActual < edadMinima) {
+            edadMinima = edadActual
+        }
+    }
+    println("\nEJEMPLO: $cantidad Alumnos")
+    println("=== Edades ===")
+    println("Edad máxima: $edadMaxima")
+    println("Edad mínima: $edadMinima")
+
+    //imprimimos todas las edades:
+    println("Todos = ${todasLasEdades.dropLast(2)}")
+
+}
 fun main() {
     print("Ingrese la cantidad de alumnos del salon: ")
     val cantidad: Int = readln().toInt()
-    ingreseAlumnos(cantidad)
+    //ingreseAlumnos(cantidad)
+    realizarEstadisticaSinListas(cantidad)
 }
+
 
