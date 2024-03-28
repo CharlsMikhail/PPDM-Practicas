5e227a89274eeda9b9e5cf04354a870f0d819b14
Carlos Mijail Mamani Anccasi <cmamania@ulasalle.edu.pe>
2024-03-28 18:43:58 -0500

    Ejercicio 3 de la practica 2 mejorado! Se encapsulo ademas se idento de manera que sea mas lejible.

--- PRACTICA 2/src/3-SistemaDeGestionDeEmpleados.kt
+++ PRACTICA 2/src/3-SistemaDeGestionDeEmpleados.kt
@@ -2,29 +2,66 @@
  * @problemDescription Se solicita implementar un sistema de gestión de empleados aplicando herencia y polimorfismo.
  * @author Carlos Mijail Mamani Anccasi
  * @creationDate 24/03/24
- * @lastModification ?
+ * @lastModification 28/03/24
  */
 
-abstract class Empleado(var nombre: String, var apellido: String, var edad: Int, var salario: Float) {
-    abstract fun calcularPago(): Float
+// Agregamos horasTrabajadas y tarifaHora como atributos de esta clase abstracta ya que en los hijos se repite.
+abstract class Empleado(
+    protected val nombre: String,
+    protected val apellido: String,
+    protected var edad: Int,
+    protected var salario: Double,
+    protected var horasTrabajadas: Int,
+    protected var tarifaHora: Double
+) {
+    abstract fun calcularPago(): Double
+    fun fetchNombre(): String = nombre
 }
-class EmpleadoTiempoCompleto(nombre: String, apellido: String, edad: Int, salario: Float, var horasTrabajadas: Int, var tarifaHora: Float): Empleado(nombre, apellido, edad, salario) {
-    override fun calcularPago(): Float {
-        return horasTrabajadas * tarifaHora
-    }
+
+class EmpleadoTiempoCompleto(
+    nombre: String,
+    apellido: String,
+    edad: Int,
+    salario: Double,
+    horasTrabajadas: Int,
+    tarifaHora: Double
+) : Empleado(nombre, apellido, edad, salario, horasTrabajadas, tarifaHora) {
+    override fun calcularPago(): Double = horasTrabajadas * tarifaHora
 }
-class EmpleadoMedioTiempo(nombre: String, apellido: String, edad: Int, salario: Float, var horasTrabajadas: Int, var tarifaHora: Float, var diasTrabajados: Int): Empleado(nombre, apellido, edad, salario) {
+
+class EmpleadoMedioTiempo(
+    nombre: String,
+    apellido: String,
+    edad: Int,
+    salario: Double,
+    horasTrabajadas: Int,
+    tarifaHora: Double,
+    private var diasTrabajados: Int
+) : Empleado(nombre, apellido, edad, salario, horasTrabajadas, tarifaHora) {
     // De los días trabajados se toman 4 horas, ya que es un empleado de medio tiempo.
-    override fun calcularPago(): Float {
-        return((horasTrabajadas + (diasTrabajados * 4)) * tarifaHora)
-    }
+    override fun calcularPago(): Double = (horasTrabajadas + (diasTrabajados * 4)) * tarifaHora
 }
 
 fun main() {
-    // Realizamos las pruebas para contrastar que la implementación sea el correcto.
-    val empleado1 = EmpleadoTiempoCompleto("Carlos", "Mamani", 20, 10295F, 58, 25F)
-    println("El pago del empleado ${empleado1.nombre} es: S./${empleado1.calcularPago()}")
+    // Realizamos las pruebas para contrastar que la implementación sea la correcta.
+    val empleado1 = EmpleadoTiempoCompleto(
+        "Carlos",
+        "Mamani",
+        20,
+        2500.0,
+        58,
+        25.0
+    )
+    println("El pago estimado del empleado ${empleado1.fetchNombre()} es: S./${empleado1.calcularPago()}")
 
-    val empleado2 = EmpleadoMedioTiempo("Abel", "Aragon", 22, 10225F, 52, 20F, 5)
-    println("El pago del empleado ${empleado2.nombre} es: S./${empleado2.calcularPago()}")
+    val empleado2 = EmpleadoMedioTiempo(
+        "Abel",
+        "Aragon",
+        22,
+        1050.0,
+        52,
+        20.0,
+        5
+    )
+    println("El pago estimado del empleado ${empleado2.fetchNombre()} es: S./${empleado2.calcularPago()}")
 }
