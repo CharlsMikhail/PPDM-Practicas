c01c2745ce2cb5f46eae4952fb18afc9ad5bdde3
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2024-04-09 23:56:18 -0500

    update MainActivity.kt, agregamos los datos generales

--- PRACTICA 3/1_InteraccionConImagenes/app/src/main/java/com/example/a1_interaccionconimagenes/MainActivity.kt
+++ PRACTICA 3/1_InteraccionConImagenes/app/src/main/java/com/example/a1_interaccionconimagenes/MainActivity.kt
@@ -3,6 +3,14 @@ package com.example.a1_interaccionconimagenes
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * @problemDescription Se solicita, implementar un boton que contenga una imagen y al presionar esta
+ * se muestre en pantalla un mensaje personalizado usando Toast.
+ * @author Carlos Mijail Mamani Anccasi
+ * @creationDate 09/04/24
+ * @lastModification 09/04/24
+ */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
