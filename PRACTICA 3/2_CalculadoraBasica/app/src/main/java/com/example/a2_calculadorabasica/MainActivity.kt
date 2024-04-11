2c28f8f4adf99b5e9e6ea82a86d47d54223a0b08
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2024-04-11 18:13:01 -0500

    update MainActivity.kt, continuamos con la logica de la calculadora, ya casi!!

--- PRACTICA 3/2_CalculadoraBasica/app/src/main/java/com/example/a2_calculadorabasica/MainActivity.kt
+++ PRACTICA 3/2_CalculadoraBasica/app/src/main/java/com/example/a2_calculadorabasica/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a2_calculadorabasica
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -17,14 +18,17 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    var exp1 = false
-    var exp2 = false
+    var exp1: Int = 0
+    var exp2: Int = 0
+    var exp2Enable = false
+    var operator = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
+    @SuppressLint("SetTextI18n")
     fun onButtonClick(view: View) {
         val buttonText = (view as Button).text.toString()
         val txtInput = findViewById<TextView>(R.id.txt_input)
@@ -33,13 +37,15 @@ class MainActivity : AppCompatActivity() {
 
         when (buttonText) {
             in "0123456789" -> {
+                if (exp2Enable == true) exp2 = currentInput.toInt()
                 txtInput.text = currentInput + buttonText
             }
             "+" -> {
-                exp1 = true
                 val currentText = txtInput.text.toString()
                 val newText = currentText + buttonText
+                exp1 = currentText.toInt()
                 txtInput.text = newText
+                operator = "+"
             }
             "-" -> {
                 val currentText = txtInput.text.toString()
@@ -58,10 +64,19 @@ class MainActivity : AppCompatActivity() {
             }
             "=" -> {
                 val expression = txtInput.text.toString()
-                val result = 1 + 1
+                val result = eval(exp1, exp2, operator)
                 txtOutput.text = "= $result"
                 txtInput.text = ""
             }
+        }
+    }
+
+    fun eval(num1: Int, num2: Int, operator: String): Int {
+        return when(operator) {
+            "+" -> num1 + num2
+            "-" -> num1 - num2
+            "*" -> num1 * num2
+            else -> num1 / num2
         }
     }
 }
