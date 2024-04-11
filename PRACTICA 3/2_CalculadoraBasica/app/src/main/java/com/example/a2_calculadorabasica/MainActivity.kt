d7af7bdab7d812313435d2c55a451098a968e72f
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2024-04-11 13:20:45 -0500

    update MainActivity.kt, Empezamos con la logica

--- PRACTICA 3/2_CalculadoraBasica/app/src/main/java/com/example/a2_calculadorabasica/MainActivity.kt
+++ PRACTICA 3/2_CalculadoraBasica/app/src/main/java/com/example/a2_calculadorabasica/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.a2_calculadorabasica
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+
 
 /**
  * @problemDescription Se solicita, crear una calculadora básica, que sume, reste, multplique
@@ -12,8 +16,57 @@ import androidx.appcompat.app.AppCompatActivity
  */
 
 class MainActivity : AppCompatActivity() {
+
+    var exp1 = false
+    var exp2 = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+
+    fun onButtonClick(view: View) {
+        val buttonText = (view as Button).text.toString()
+        val txtInput = findViewById<TextView>(R.id.txt_input)
+        val txtOutput = findViewById<TextView>(R.id.txt_output)
+
+        when (buttonText) {
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" -> {
+                val currentText = txtInput.text.toString()
+                val newText = currentText + buttonText
+                txtInput.text = newText
+            }
+            "+" -> {
+                val currentText = txtInput.text.toString()
+                val newText = currentText + " + "
+                txtInput.text = newText
+            }
+            "-" -> {
+                val currentText = txtInput.text.toString()
+                val newText = currentText + " - "
+                txtInput.text = newText
+            }
+            "*" -> {
+                val currentText = txtInput.text.toString()
+                val newText = currentText + " * "
+                txtInput.text = newText
+            }
+            "/" -> {
+                val currentText = txtInput.text.toString()
+                val newText = currentText + " / "
+                txtInput.text = newText
+            }
+            "=" -> {
+                val expression = txtInput.text.toString()
+                val result = 1 + 1
+                txtOutput.text = "= $result"
+                txtInput.text = ""
+            }
+        }
     }
 }
+
+
+
