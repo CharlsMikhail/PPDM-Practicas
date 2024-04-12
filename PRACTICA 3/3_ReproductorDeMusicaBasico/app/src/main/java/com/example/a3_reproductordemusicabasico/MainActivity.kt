1e92981dafd6ada9bb6dc5ac0076a07597dc8b21
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2024-04-12 00:45:02 -0500

    Avanzado con la logica!!

--- PRACTICA 3/3_ReproductorDeMusicaBasico/app/src/main/java/com/example/a3_reproductordemusicabasico/MainActivity.kt
+++ PRACTICA 3/3_ReproductorDeMusicaBasico/app/src/main/java/com/example/a3_reproductordemusicabasico/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.a3_reproductordemusicabasico
 
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -12,8 +17,37 @@ import androidx.appcompat.app.AppCompatActivity
  */
 
 class MainActivity : AppCompatActivity() {
+
+    private val play_pause : ImageButton? = null
+    private val boton_repetir: ImageButton? = null
+    private val mediaPlayer: MediaPlayer? = null
+    private val imagenView: ImageView? = null
+    private val repetir = 2
+    private val posicion = 0
+
+    private val mediaPlayerArray = Array<MediaPlayer?>(3) { null }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val play_pause = findViewById<ImageButton>(R.id.btn_play)
+
+        val iv = findViewById<ImageView>(R.id.imageView)
+
+        mediaPlayerArray[0] = MediaPlayer.create(this, R.raw.creep);
+        mediaPlayerArray[1] = MediaPlayer.create(this, R.raw.cumbia);
+        mediaPlayerArray[2] = MediaPlayer.create(this, R.raw.foster);
     }
+
+    fun playPause(view: View) {
+        if (mediaPlayerArray[posicion]?.isPlaying() == true) {
+            mediaPlayerArray[posicion]?.pause()
+            play_pause?.setBackgroundResource(R.drawable.boton_de_pausa)
+            Toast
+        } else {
+
+        }
+    }
+
 }
