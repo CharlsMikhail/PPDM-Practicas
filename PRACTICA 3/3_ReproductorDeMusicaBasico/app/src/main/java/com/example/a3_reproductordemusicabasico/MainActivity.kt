8cb5e2fd0faa5a449a5699765b4dcd4538ec804f
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2024-04-12 02:33:59 -0500

    Update all, los metodos funcionale a los botones(siguiente, anterior, parar y play), se agrego funcionalidad a repetir2.png. Se agrego string para los titulos de cada cancion.

--- PRACTICA 3/3_ReproductorDeMusicaBasico/app/src/main/java/com/example/a3_reproductordemusicabasico/MainActivity.kt
+++ PRACTICA 3/3_ReproductorDeMusicaBasico/app/src/main/java/com/example/a3_reproductordemusicabasico/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -22,10 +23,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var posicion = 0
+        var repetir = 0
 
-        val play_pause = findViewById<ImageButton>(R.id.btn_play)
-        val posicion = 0
         val iv = findViewById<ImageView>(R.id.imageView)
+        val play_pause = findViewById<ImageButton>(R.id.btn_play)
+        val btn_repetir = findViewById<ImageButton>(R.id.btn_repetir)
+        val btn_stop = findViewById<ImageButton>(R.id.btn_detener)
+        val btn_anterior = findViewById<ImageButton>(R.id.btn_anterior)
+        val btn_siguiente = findViewById<ImageButton>(R.id.btn_siguiente)
+        val txt_titulo = findViewById<TextView>(R.id.txt_titulo)
 
         mediaPlayerList = listOf(
             MediaPlayer.create(this, R.raw.creep),
@@ -33,18 +40,105 @@ class MainActivity : AppCompatActivity() {
             MediaPlayer.create(this, R.raw.foster)
         )
 
-
+        // Método para el boton PLAY & PAUSE
         play_pause.setOnClickListener() {
             if (mediaPlayerList[posicion].isPlaying()) {
                 mediaPlayerList[posicion].pause()
                 play_pause.setImageResource(R.drawable.boton_de_play)
-                Toast.makeText(this, "Pausa", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, " Pausa", Toast.LENGTH_SHORT).show()
             } else {
                 mediaPlayerList[posicion].start()
                 play_pause.setImageResource(R.drawable.boton_de_pausa)
-                Toast.makeText(this, "Play", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "   Play", Toast.LENGTH_SHORT).show()
             }
         }
+
+        // Método para el boton DETENER
+        btn_stop.setOnClickListener() {
+            mediaPlayerList[posicion].stop()
+            mediaPlayerList = listOf(
+                MediaPlayer.create(this, R.raw.creep),
+                MediaPlayer.create(this, R.raw.cumbia),
+                MediaPlayer.create(this, R.raw.foster)
+            )
+            posicion = 0
+            play_pause.setImageResource(R.drawable.boton_de_play)
+            iv.setImageResource(R.drawable.creep_port)
+            txt_titulo.setText(R.string.txt_creep)
+            Toast.makeText(this, "   Stop", Toast.LENGTH_SHORT).show()
+        }
+
+        btn_repetir.setOnClickListener() {
+            if (repetir == 1) {
+                btn_repetir.setImageResource(R.drawable.repetir)
+                Toast.makeText(this, "No repetir", Toast.LENGTH_SHORT).show()
+                mediaPlayerList[posicion].isLooping = false
+                repetir = 2
+            } else {
+                btn_repetir.setImageResource(R.drawable.repetir2)
+                Toast.makeText(this, "Repetir", Toast.LENGTH_SHORT).show()
+                mediaPlayerList[posicion].isLooping = true
+                repetir = 1
+            }
+        }
+
+        btn_siguiente.setOnClickListener() {
+            if (posicion < mediaPlayerList.size - 1) {
+                if (mediaPlayerList[posicion].isPlaying()) {
+                    mediaPlayerList[posicion].stop();
+                    posicion++
+                    mediaPlayerList[posicion].start()
+                } else {
+                    posicion ++
+                }
+                when(posicion) {
+                    0 -> {iv.setImageResource(R.drawable.creep_port)
+                        txt_titulo.setText(R.string.txt_creep)
+                    }
+                    1 -> {iv.setImageResource(R.drawable.cumbia_port)
+                        txt_titulo.setText(R.string.txt_creep)
+                    }
+                    else -> {
+                        iv.setImageResource(R.drawable.foster_port)
+                        txt_titulo.setText(R.string.txt_creep)
+                    }
+                }
+            } else {
+                Toast.makeText(this, "No hay mas canciones", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        btn_anterior.setOnClickListener() {
+            if (posicion >= 1) {
+                if (mediaPlayerList[posicion].isPlaying()) {
+                    mediaPlayerList[posicion].stop();
+                    mediaPlayerList = listOf(
+                        MediaPlayer.create(this, R.raw.creep),
+                        MediaPlayer.create(this, R.raw.cumbia),
+                        MediaPlayer.create(this, R.raw.foster)
+                    )
+                    posicion--
+                    mediaPlayerList[posicion].start()
+                } else {
+                    posicion --
+                }
+                when(posicion) {
+                    0 -> {iv.setImageResource(R.drawable.creep_port)
+                        txt_titulo.setText(R.string.txt_creep)
+                    }
+                    1 -> {iv.setImageResource(R.drawable.cumbia_port)
+                        txt_titulo.setText(R.string.txt_creep)
+                    }
+                    else -> {
+                        iv.setImageResource(R.drawable.foster_port)
+                        txt_titulo.setText(R.string.txt_creep)
+                    }
+                }
+            } else {
+                Toast.makeText(this, "No hay mas canciones", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 
 }
