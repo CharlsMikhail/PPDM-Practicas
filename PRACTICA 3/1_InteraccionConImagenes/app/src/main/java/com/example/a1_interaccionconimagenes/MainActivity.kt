28a3e5d236977a9f37c799f4d8c1a995f3d1fba3
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2024-04-13 19:57:13 -0500

    Ya conectamos la imagen con el Mensaje Personalzado

--- PRACTICA 3/1_InteraccionConImagenes/app/src/main/java/com/example/a1_interaccionconimagenes/MainActivity.kt
+++ PRACTICA 3/1_InteraccionConImagenes/app/src/main/java/com/example/a1_interaccionconimagenes/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.a1_interaccionconimagenes
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -15,5 +18,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val txt_custom = findViewById<EditText>(R.id.txt_custom)
+        val btn_imgb = findViewById<ImageButton>(R.id.imgb_main)
+        btn_imgb.setOnClickListener() {
+            //val msg = "Acabas de hacer click en la imagen :)"
+            Toast.makeText(this, txt_custom.text, Toast.LENGTH_SHORT).show()
+        }
     }
 }
