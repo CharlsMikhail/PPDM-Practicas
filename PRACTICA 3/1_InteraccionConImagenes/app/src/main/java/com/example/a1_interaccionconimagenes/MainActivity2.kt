95868ea78e3af3cd8766887f0aec94dbf50e83f8
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2024-04-20 08:28:31 -0500

    update MainActivity2.kt, muestra la imgaen selccionada y ademas al volver empaqueta la información.

--- PRACTICA 3/1_InteraccionConImagenes/app/src/main/java/com/example/a1_interaccionconimagenes/MainActivity2.kt
+++ PRACTICA 3/1_InteraccionConImagenes/app/src/main/java/com/example/a1_interaccionconimagenes/MainActivity2.kt
@@ -1,7 +1,11 @@
 package com.example.a1_interaccionconimagenes
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -12,15 +16,34 @@ class MainActivity2 : AppCompatActivity() {
         setContentView(R.layout.activity_main2)
 
         // Recupero las información de la actividad 1
-        val txt = intent.getStringExtra(KEY_TEXT)
+        val mensaje = intent.getStringExtra(KEY_TEXT)
         val imgSelected = intent.getStringExtra(KEY_IMG)
 
         val imgMain = findViewById<ImageButton>(R.id.imgb_main)
-        when (imgSelected) {
-            "imagen1" ->
+
+        imgMain.setImageResource(
+            when (imgSelected) {
+                "imagen1" -> R.drawable.imagen1
+                "imagen2" -> R.drawable.imagen2
+                "imagen3" -> R.drawable.imagen3
+                else -> {TODO("EXCEPTIONS")}
+            }
+        )
+
+        imgMain.setOnClickListener {
+            Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
         }
 
+        val btnBack = findViewById<Button>(R.id.btn_return)
 
+        btnBack.setOnClickListener {
+            // Empaquetamos antes de volver, apra poder recuperar la información.
+            val intent = Intent()
+            intent.putExtra(KEY_TEXT, mensaje)
+            intent.putExtra(KEY_IMG, imgSelected)
+            setResult(Activity.RESULT_OK, intent)
 
+            finish()
+        }
     }
 }
