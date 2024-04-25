df9b6bef15c21803e6fe0e5c56b1dcf87b8dfb98
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2024-04-25 11:59:18 -0500

    Avanzamos con la logica menu fragment, preparamos para el cambio de fragmento.

--- PRACTICA 5/1_InteraccionConImagenesV3/app/src/main/java/com/example/a1_interaccionconimagenesv3/MenuFragment.kt
+++ PRACTICA 5/1_InteraccionConImagenesV3/app/src/main/java/com/example/a1_interaccionconimagenesv3/MenuFragment.kt
@@ -1,11 +1,41 @@
 package com.example.a1_interaccionconimagenesv3
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
+
+const val REQUEST_CODE = "123"
+const val KEY_TEXT = "text"
+const val KEY_IMG = "image"
+
 class MenuFragment : Fragment(R.layout.fragment_menu) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val txtCustom = view.findViewById<EditText>(R.id.txt_custom)
+        val spnImages = view.findViewById<Spinner>(R.id.spn_images)
+        val btnSelect = view.findViewById<Button>(R.id.btn_select)
+
+        val options = listOf("Corazon", "Retrato", "Goku")
+        val adapter = ArrayAdapter(requireContext(), R.layout.spinner_item_custom, options)
+        spnImages.adapter = adapter
+
+        btnSelect.setOnClickListener {
+            // Preparamos el cambio de actividad
+            val msg = txtCustom.text.toString()
+            val selected = spnImages.selectedItemPosition
+
+            requireActivity().supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentMenu, ImageFragment.newInstance(msg, selected))
+                addToBackStack(REQUEST_CODE) // manda a la pila a el fragmento antes de remplazarlo.
+                commit()
+            }
+
+        }
     }
 }
