ca6b947ba0829b63107758abdc3ba115ac266b86
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2024-04-26 01:38:29 -0500

    Agregamos las interfaces graficas que ya implmentamos con anterioridad.

---
+++ PRACTICA 5/2_ReproductorDeMusicaV3/app/src/main/java/com/example/a2_reproductordemusicav3/MenuMusicFragment.kt
@@ -0,0 +1,11 @@
+package com.example.a2_reproductordemusicav3
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+
+class MenuMusicFragment : Fragment(R.layout.fragment_menu_music) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ PRACTICA 5/2_ReproductorDeMusicaV3/app/src/main/java/com/example/a2_reproductordemusicav3/ReproductorFragment.kt
@@ -0,0 +1,11 @@
+package com.example.a2_reproductordemusicav3
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+
+class ReproductorFragment : Fragment(R.layout.fragment_reproductor) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
